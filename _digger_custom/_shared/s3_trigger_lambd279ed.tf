fc02c1fd34c35fff148a1ba61843543f517cb152
motatoes <moe.habib9@gmail.com>
2022-03-09 15:25:27 +0000

    fix aws lambda function name

--- _digger_custom/_shared/s3_trigger_lambd279ed.tf
+++ _digger_custom/_shared/s3_trigger_lambd279ed.tf
@@ -25,7 +25,7 @@ resource "aws_s3_bucket_notification" "bucket_notification" {
   bucket = aws_s3_bucket.custom_s3_bucket.id
 
   lambda_function {
-    lambda_function_arn = aws_lambda_function.lambd279ed.arn
+    lambda_function_arn = data.aws_lambda_function.lambd279ed.arn
     # events              = ["s3:ObjectCreated:*"]
     # filter_prefix       = "AWSLogs/"
     # filter_suffix       = ".log"
